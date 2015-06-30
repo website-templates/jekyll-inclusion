34385476b83722945af7f0fec525365993a5ed98
Maxim Orlov <mail@orlovmax.com>
2015-06-30 23:24:29 +0300

    publ script modified

--- _publ/post.sh
+++ _publ/post.sh
@@ -1,4 +1,5 @@
 cp -a pages/_drafts/_draft.md pages/_posts/$@.md;
+sed -i "s/_PATH/$@/g" "$(pwd -W | sed  's/\//\\/g')\pages\_posts\\$@.md";
 explorer "$(pwd -W | sed  's/\//\\/g')\pages\_posts\\$@.md";
 mkdir img/post/$@;
 explorer "$(pwd -W | sed  's/\//\\/g')\img\post\\$@";
